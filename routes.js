0604ba05f6c5cf33b527f16c49329728fa2fe68c
Elisa Amaral <mail.amaralelisa@gmail.com>
2022-12-08 15:51:33 -0300

    Update routes.js Router const name

--- routes.js
+++ routes.js
@@ -1,24 +1,24 @@
 const express = require('express')
-const route = express.Router()
+const router = express.Router()
 const homeController = require('./src/controllers/homeController')
 const authenticationController = require('./src/controllers/authenticationController')
 const contactController = require('./src/controllers/contactController')
 const { loginRequired } = require('./src/middlewares/middleware')
 
 // Home routes
-route.get('/', homeController.index)
+router.get('/', homeController.index)
 
 // Login routes
-route.get('/authentication/index', authenticationController.index)
-route.post('/authentication/login', authenticationController.login)
-route.post('/authentication/signup', authenticationController.signup)
-route.get('/authentication/logout', authenticationController.logout)
+router.get('/authentication/index', authenticationController.index)
+router.post('/authentication/login', authenticationController.login)
+router.post('/authentication/signup', authenticationController.signup)
+router.get('/authentication/logout', authenticationController.logout)
 
 // Contact routes
-route.get('/contact/index', loginRequired, contactController.index)
-route.post('/contact/add', loginRequired, contactController.add)
-route.get('/contact/index/:id', loginRequired, contactController.editAfterAdding)
-route.post('/contact/edit/:id', loginRequired, contactController.edit)
-route.get('/contact/delete/:id', loginRequired, contactController.delete)
+router.get('/contact/index', loginRequired, contactController.index)
+router.post('/contact/add', loginRequired, contactController.add)
+router.get('/contact/index/:id', loginRequired, contactController.editAfterAdding)
+router.post('/contact/edit/:id', loginRequired, contactController.edit)
+router.get('/contact/delete/:id', loginRequired, contactController.delete)
 
-module.exports = route
+module.exports = router
